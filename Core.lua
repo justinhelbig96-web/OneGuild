64a630049c691d12c2bea52caf4a7b9cdffb922f
justinhelbig96-web <justinhelbig96-web@users.noreply.github.com>
2026-03-19 20:09:20 +0100

    Bump version to 1.1.2

--- Core.lua
+++ Core.lua
@@ -18,7 +18,7 @@ OneGuild.REQUIRED_GUILD = "One"
 ------------------------------------------------------------------------
 -- Version & Constants
 ------------------------------------------------------------------------
-OneGuild.VERSION = "1.1.1"
+OneGuild.VERSION = "1.1.2"
 
 ------------------------------------------------------------------------
 -- Admin Whitelist  –  now loaded from SavedVariables (db.settings.whitelist)
