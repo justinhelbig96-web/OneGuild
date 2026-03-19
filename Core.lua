5196b247f647b26b267b2192d6f83e54594f3bb3
justinhelbig96-web <justinhelbig96-web@users.noreply.github.com>
2026-03-19 13:23:58 +0100

    Bump version to 1.0.1

--- Core.lua
+++ Core.lua
@@ -18,7 +18,7 @@ OneGuild.REQUIRED_GUILD = "One"
 ------------------------------------------------------------------------
 -- Version & Constants
 ------------------------------------------------------------------------
-OneGuild.VERSION = "1.0.0"
+OneGuild.VERSION = "1.0.1"
 
 ------------------------------------------------------------------------
 -- Admin Whitelist  –  Characters listed here get auto-admin rights.
